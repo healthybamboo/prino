d9de0a6e5056aa28e788fb131d16a0d29da60250
Takeru Sakuma <healthybamboo@bamgrove.net>
2023-12-15 07:19:28 +0900

    ✨ add admin

--- apps/primo/admin.py
+++ apps/primo/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Movie, Job
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    pass
