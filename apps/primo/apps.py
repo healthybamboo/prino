66a1b05b570250d61cb3f8c59e449de2c1092788
Takeru Sakuma <healthybamboo@bamgrove.net>
2023-12-15 11:39:36 +0900

    ✨ Add auto load

--- apps/primo/apps.py
+++ apps/primo/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class PrimoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.primo'
+
+    def ready(self) -> None:
+        import apps.primo.signals
+        from apps.primo.application import CheckService
+
+        check_service = CheckService()
+        check_service.run()
