787e2e5fba9edade8168f199c26516c4b9290143
Takeru Sakuma <healthybamboo@bamgrove.net>
2023-12-15 18:13:47 +0900

    🔒️ Fix credential

--- apps/primo/application/notify_service.py
+++ apps/primo/application/notify_service.py
@@ -2,6 +2,7 @@ import logging
 import requests
 import json
 from datetime import datetime
+from django.conf import settings
 from apps.primo.models import Movie
 
 
@@ -18,7 +19,7 @@ class NotifyService(object):
         }
 
     def _send_discord_notify(self, movies: Movie):
-        discord_notify_webhook = 'https://discord.com/api/webhooks/1185016466504949860/ixkmEOSfy-13wSQpvEcQJVfmGsPvM4ztuzlG7iFZLLCS-MaMkfGUU25VPnBskFaEH8Yi'
+        discord_notify_webhook = settings.DISCORD_WEBHOOK_URL
         headers = self._get_headers()
         print('send_discord_notify')
 
