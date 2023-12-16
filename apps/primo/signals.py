8759e5dfeed4c1d4684ccf65c3f6d8c53506ecc1
Takeru Sakuma <healthybamboo@bamgrove.net>
2023-12-16 11:39:18 +0900

    🩹 Fix signals

--- apps/primo/signals.py
+++ apps/primo/signals.py
@@ -1,16 +1,35 @@
+from dateutil.relativedelta import relativedelta
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from .models import Movie
-from apps.primo.application import CheckService
+from apps.primo.application.movie_service import MovieService
 from apps.primo.application import NotifyService
 
-check_service = CheckService()
+movie_service = MovieService()
 notify_service = NotifyService()
 
 
 @receiver(post_save, sender=Movie)
 def listen_post_save(sender, instance, created, **kwargs):
     if created:
-        check_service.initMovie()
-        notify_service.send_register_message(instance)
+        movie = instance
+        try:
+            (
+                episode,
+                episode_title,
+                image,
+                title,
+                first_posted_date,
+            ) = movie_service.get_movie_info(movie.url)
+            movie.title = movie.title or title
+            movie.episode = movie.episode or episode
+            movie.episode_title = movie.episode_title or episode_title
+            movie.end_at = first_posted_date + relativedelta(months=+3)
+            movie.image = movie.image or image
+            movie.status = 'completed'
+            movie.save()
+        except Exception as e:
+            print(e)
+            movie.status = 'failed'
+            movie.save()
+        notify_service.send_register_message(movie)
