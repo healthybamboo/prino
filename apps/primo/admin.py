5f1f54e3d71a91d089b28d51baf3cc37fbe6d2fb
Takeru Sakuma <healthybamboo@bamgrove.net>
2023-12-16 11:40:44 +0900

    🚸 Fix admin

--- apps/primo/admin.py
+++ apps/primo/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.utils.html import mark_safe
 from .models import Category, Movie
+from django.forms import Select, TextInput, ModelForm
 
 
 # Register your models here.
@@ -22,10 +23,23 @@ class MovieAdmin(admin.ModelAdmin):
         'preview',
     )
 
+    def get_form(self, request, obj=None, **kwargs):
+        if obj is None:
+            self.exclude = [
+                'status',
+                'title',
+                'image',
+                'episode',
+                'episode_title',
+                'end_at',
+                'is_active',
+            ]
+        return super().get_form(request, obj, **kwargs)
+
     def get_readonly_fields(self, request, obj=None):
         if obj is None:
             return []
-        return ['status', 'title', 'image']
+        return ['status', 'title', 'image', 'episode', 'episode_title', 'url']
 
     def preview(self, obj):
         return mark_safe(f'<img src="{obj.image}" width="150px" />')
