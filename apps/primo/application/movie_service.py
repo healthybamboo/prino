d094fa04fd0717c59391ea00372e765c4b0d6733
Takeru Sakuma <healthybamboo@bamgrove.net>
2023-12-19 15:25:24 +0900

    🩹 Fix requests

--- apps/primo/application/movie_service.py
+++ apps/primo/application/movie_service.py
@@ -15,8 +15,7 @@ class MovieService:
 
     def _get_res(self, url: str):
         headers = self._get_headers()
-        sesssion = requests.Session()
-        res = sesssion.get(url, headers=headers)
+        res = requests.get(url, headers=headers)
         if res.status_code != 200:
             raise Exception(f'error: {res.status_code} \n {url} \n {res.text}')
         return res
