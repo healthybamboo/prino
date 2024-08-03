35f98cfa16d2eed5cb3221c5cf71fd87733ec759
Takeru Sakuma <healthybamboo@bamgrove.net>
2024-08-03 18:50:49 +0900

    Fix run message

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # === ORMのマーグレーション処理 ===
-echo マイグレーションを実行...
+echo マイグレーションを実行中...
 python manage.py migrate --settings=config.settings.production
 echo マイグレーションを完了
 
 # === 以下、Djangoの起動処理 ===
-echo Djangoアプリケーションを起動...
+echo Djangoアプリケーションを起動中...
 gunicorn config.wsgi:applicatio -c config/gunicorn.py
-echo Djangoアプリケーションを起動しました
+echo Djangoアプリケーションの起動を完了
