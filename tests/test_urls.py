2eff9523a8b029a462ab2162635c7b89300a1a4a
Takeru Sakuma <healthybamboo@bamgrove.net>
2024-08-03 17:46:13 +0900

    Fix tests

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -4,8 +4,9 @@ from django.core.management import call_command
 from django.urls import resolve
 from django.contrib.admin import AdminSite
 
+
 # 正しく、admin.site.urls が呼び出されていることを確認するテスト
 def test_can_access_top_page():
-    view = resolve('/admin/')
+    view = resolve('/admin')
     assert view.func.__module__ == 'django.contrib.admin.sites'
     assert view.func.__name__ == 'index'
