bf76b78292763df15b2f8bd5f634db1ffe1cd5e3
Takeru Sakuma <healthybamboo@bamgrove.net>
2024-08-03 10:53:50 +0900

    Fix dev conf

--- config/settings/develop.py
+++ config/settings/develop.py
@@ -1,3 +1,14 @@
 from .base import *
 
 DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/xxxxxxxxxxxxx'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'host.docker.internal',
+        'PORT': '5432',
+        'NAME': 'prino',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+    }
+}
